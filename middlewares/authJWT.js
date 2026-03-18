1dd0a87ae3d3bdbbf9276f1f7e393db4dcdfd7b8
Julio Dalpiaz <juliodalpiaz>
2026-03-17 20:15:40 -0400

    Remove console.log

--- middlewares/authJWT.js
+++ middlewares/authJWT.js
@@ -7,18 +7,16 @@ module.exports = function authJWT(req, res, next) {
         return res.status(401).json({ message: "Token não enviado." });
     }
 
-    const token = authHeader.split(" ")[1]; // "Bearer <token>"
+    const token = authHeader.split(" ")[1];
 
-    console.log("Authorization header:", authHeader);
-    console.log("Token extraído:", token);
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("Token decodificado:", decoded);
         req.user = decoded;
-
         next();
     } catch (err) {
-        console.error("JWT inválido:", err);
+        if (process.env.NODE_ENV !== "production") {
+            console.error("JWT inválido:", err);
+        }
         return res.status(401).json({ message: "Token inválido ou expirado." });
     }
 };
